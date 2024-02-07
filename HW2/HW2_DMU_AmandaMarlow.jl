5e307a8dc5cbfa655361a01530c5becedb247c4d
amandamarlow <77698771+amanda1797@users.noreply.github.com>
2024-02-06 22:56:33 -0700

    attempts at other algorithms commented out + good Q4 code still

--- HW2/HW2_DMU_AmandaMarlow.jl
+++ HW2/HW2_DMU_AmandaMarlow.jl
@@ -55,13 +55,20 @@ display(render(grid_world, color=V3))
 # Question 4
 ############
 
-function checkDist(state)
+function checkDist(state)::Float64
     if (state[4] <=100) && (abs(state[1]-state[3])<=500)
-        return -100;
+        return -100.0;
+        @show state
     else
-        return 0;
+        return 0.0;
     end
 end
+
+# function checkDist(state)::Float64
+#     value = -(1/state[4]+5/abs(state[1]-state[3]))
+#     @show value
+#     return value
+# end
 
 function ACAS_value_iteration(m, discount)
     
@@ -76,23 +83,38 @@ function ACAS_value_iteration(m, discount)
     V = rand(Float64, n)
     Vnext = checkDist.(states(m))
     
-    while norm(V-Vnext, 2) > e
-        V[:] = Vnext
-        Voptions = Array{Float64, 2}(undef,length(A), n)
-        q = 1
-        for a in A #ordered_actions(m)
-            Voptions[q,:] = R[a] + discount*T[a]*V
-            q = q+1
+    # while norm(V-Vnext, 2) > e
+    #     V[:] = Vnext
+    #     Voptions = Array{Float64, 2}(undef,length(A), n)
+    #     q = 1
+    #     for a in A #ordered_actions(m)
+    #         Voptions[q,:] = R[a] + discount*T[a]*V
+    #         q = q+1
+    #     end
+    #     Vnext[:] = maximum(Voptions, dims=1)
+    # end
+
+    U = checkDist.(states(m))
+    for k = 1:20
+        Uoptions = Array{Float64, 1}(undef,length(A))
+        for(i, s) in enumerate(states(m))
+            for (q, a) in enumerate(A) #ordered_actions(m)
+                future = discount*T[a]*U;
+                Uoptions[q] = R[a][i] + future[i];
+            end
+            U[i] = maximum(Uoptions)
         end
-        Vnext[:] = maximum(Voptions, dims=1)
     end
-    V[:] = Vnext
+
+    # V[:] = Vnext
+    V[:] = U
 
     return V
 end
 
-n = 10
+n = 4
 m = HW2.UnresponsiveACASMDP(n)
 V4 = ACAS_value_iteration(m, 0.99)
 
+# @show HW2.evaluate(V4)
 @show HW2.evaluate(V4, "amanda.marlow@colorado.edu")
