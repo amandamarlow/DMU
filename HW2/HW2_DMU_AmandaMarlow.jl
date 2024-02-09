eac65ad8cd54d0f0a8de18dc18cd611c395e07ea
amandamarlow <77698771+amanda1797@users.noreply.github.com>
2024-02-09 14:18:22 -0700

    Final HW2 Submission

--- HW2/HW2_DMU_AmandaMarlow.jl
+++ HW2/HW2_DMU_AmandaMarlow.jl
@@ -19,30 +19,30 @@ display(render(SimpleGridWorld()))
 
 function value_iteration(m, discount)
     
-    e = 1e-7
-    n = length(states(m))
+e = 1e-7
+n = length(states(m))
 
-    A = collect(actions(m))
-    T = transition_matrices(m)
-    R = reward_vectors(m)
+A = collect(actions(m))
+T = transition_matrices(m)
+R = reward_vectors(m)
 
-    V = rand(Float64, n) # this would be a good container to use for your value function
-    Vnext = rand(Float64, n)
+V = rand(Float64, n) # this would be a good container to use for your value function
+Vnext = rand(Float64, n)
     
-    while norm(V-Vnext, 2) > e
-        V[:] = Vnext
-        Voptions = Array{Float64, 2}(undef,length(A), n)
-        q = 1
-        for a in A
-            Voptions[q,:] = R[a] + discount*T[a]*V
-            q = q+1
-        end
-        Vnext[:] = maximum(Voptions, dims=1)
-    end
-    V = Vnext
-    # pi = argmax(R[A] + discount*T[A]*V, A)
+while norm(V-Vnext, 2) > e
+V[:] = Vnext
+Voptions = Array{Float64, 2}(undef,length(A), n)
+q = 1
+for a in A
+Voptions[q,:] = R[a] + discount*T[a]*V
+q = q+1
+end
+Vnext[:] = maximum(Voptions, dims=1)
+end
+V = Vnext
+# pi = argmax(R[A] + discount*T[A]*V, A)
 
-    return V
+return V
 end
 
 V3 = value_iteration(grid_world, 0.95)
@@ -75,39 +75,39 @@ function ACAS_value_iteration(m, discount)
     e = 1e-7
     n = length(states(m))
 
-    # A = collect(ordered_actions(m))
-    A = collect(actions(m))
+    A = collect(ordered_actions(m))
+    # A = collect(actions(m))
     T = transition_matrices(m, sparse=true)
     R = reward_vectors(m)
 
     V = rand(Float64, n)
     Vnext = checkDist.(states(m))
     
-    # while norm(V-Vnext, 2) > e
-    #     V[:] = Vnext
-    #     Voptions = Array{Float64, 2}(undef,length(A), n)
-    #     q = 1
-    #     for a in A #ordered_actions(m)
-    #         Voptions[q,:] = R[a] + discount*T[a]*V
-    #         q = q+1
-    #     end
-    #     Vnext[:] = maximum(Voptions, dims=1)
-    # end
-
-    U = checkDist.(states(m))
-    for k = 1:20
-        Uoptions = Array{Float64, 1}(undef,length(A))
-        for(i, s) in enumerate(states(m))
-            for (q, a) in enumerate(A) #ordered_actions(m)
-                future = discount*T[a]*U;
-                Uoptions[q] = R[a][i] + future[i];
+    while norm(V-Vnext, 2) > e
+                V[:] = Vnext
+                Voptions = Array{Float64, 2}(undef,length(A), n)
+                q = 1
+            for a in A #ordered_actions(m)
+                Voptions[q,:] = R[a] + discount*T[a]*V
+                    q = q+1
             end
-            U[i] = maximum(Uoptions)
-        end
+            Vnext[:] = maximum(Voptions, dims=1)
     end
 
-    # V[:] = Vnext
-    V[:] = U
+    # U = checkDist.(states(m))
+    # for k = 1:20
+    #     Uoptions = Array{Float64, 1}(undef,length(A))
+    #     for(i, s) in enumerate(states(m))
+    #     for (q, a) in enumerate(A) #ordered_actions(m)
+    #         future = discount*T[a]*U;
+    # Uoptions[q] = R[a][i] + future[i];
+    # end
+    # U[i] = maximum(Uoptions)
+    # end
+    # end
+    
+    V[:] = Vnext
+    # V[:] = U
 
     return V
 end
@@ -116,5 +116,5 @@ n = 4
 m = HW2.UnresponsiveACASMDP(n)
 V4 = ACAS_value_iteration(m, 0.99)
 
-# @show HW2.evaluate(V4)
-@show HW2.evaluate(V4, "amanda.marlow@colorado.edu")
+@show HW2.evaluate(V4)
+# @show HW2.evaluate(V4, "amanda.marlow@colorado.edu")
