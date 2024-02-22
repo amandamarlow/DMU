0d0157a4725367c94c8cb46385be785c90243b4b
amandamarlow <77698771+amanda1797@users.noreply.github.com>
2024-02-21 17:18:38 -0700

    final code for HW3 Q5

--- HW3/HW3_5264_AmandaMarlow.jl
+++ HW3/HW3_5264_AmandaMarlow.jl
@@ -27,18 +27,20 @@ function rollout(mdp, policy_function, s0, max_steps=100)
 end
 
 
-function heuristic_policy(m, s) # similar to random :(
-    # put a smarter heuristic policy here
+function heuristic_policy(mdp, s) # similar to random :(
+    
+    gridSize = 100
+    
     remainder = mod.(s, 20)
     # @show s
     # @show remainder
     if s[1] <= 10
         a = :right
-    elseif s[1] >= 50
+    elseif s[1] >= gridSize-10
         a = :left
     elseif s[2] <= 10
         a = :up
-    elseif s[2] >= 50
+    elseif s[2] >= gridSize-10
         a = :down
     elseif (remainder[1] == 0) && (remainder[2] <= 10)
         a = :down
@@ -86,11 +88,8 @@ function get_mean_SEM(data::Vector{Float64})
 end
 
 
-m = HW3.DenseGridWorld(seed=3)
-iterations = 500
-
-# p = VectorPolicy(m, heuristic_policy.(m, states))
-# display(render(m))
+# m = HW3.DenseGridWorld(seed=3)
+# iterations = 500
 
 # results_random = [rollout(m, random_policy, rand(initialstate(m))) for _ in 1:iterations]
 # mean_random, SEM_random = get_mean_SEM(results_random)
@@ -106,83 +105,129 @@ iterations = 500
 
 
 ############
-# Question 3
+# MCTS Code
 ############
 
-S = statetype(m)
-A = actiontype(m)
-
-# These would be appropriate containers for your Q, N, and t dictionaries:
-n = Dict{Tuple{S, A}, Int}()
-q = Dict{Tuple{S, A}, Float64}()
-t = Dict{Tuple{S, A, S}, Int}()
-
-function SelectAction(mdp, s, nsims, c)
-    S = statetype(mdp)
-    A = actiontype(mdp)
-    N = Dict{Tuple{S, A}, Int}()
-    Q = Dict{Tuple{S, A}, Float64}()
-    t = Dict{Tuple{S, A, S}, Int}()
-    d = 50;
-    for k in 1:nSims
-        simulate!(mdp, s, d, c, N, Q)
-    end
-    return argmax(a->Q[(s,a)], actions(mdp))
-end
-
-function simulate!(mdp, s, d, c, N, Q, t=nothing)
+function simulate!(mdp, s, d, N, Q, T=nothing)
     A = collect(actions(mdp))
     if d <= 0
-        return rollout(mdp, heuristic_policy, s)
+        return rollout(mdp, heuristic_policy, s, d)
     elseif  !haskey(N, (s, first(A)))
         for a in A
             N[(s,a)] = 0
             Q[(s,a)] = 0.0
         end
-        return rollout(mdp, heuristic_policy, s)
+        return rollout(mdp, heuristic_policy, s, d)
     end
+    c = 200 # exploration constant
     a = explore(s, N, Q, A, c)
     s′, r = @gen(:sp, :r)(mdp, s, a)
 
-    if t !== nothing
-        t[(s, a, s′)] = get(t, (s, a, s′), 0) + 1
+    if T !== nothing
+        T[(s, a, s′)] = get(T, (s, a, s′), 0) + 1
     end
 
-    q = r + discount(mdp)*simulate!(mdp, s′, d-1, c, N, Q, t)
+    q = r + discount(mdp)*simulate!(mdp, s′, d-1, N, Q, T)
     N[(s,a)] += 1
     Q[(s,a)] += (q-Q[(s,a)])/N[(s,a)]
     return q
 end
 
 bonus(Nsa, Ns) = Nsa == 0 ? Inf : sqrt(log(Ns)/Nsa)
-
 function explore(s, N, Q, A, c)
     Ns = sum(N[(s,a)] for a in A)
     return argmax(a->Q[(s,a)] + c*bonus(N[(s,a)], Ns), A)
 end
 
-mdp = DenseGridWorld(seed=4)
-nSims = 7
-c = 200
-s = SA[19,19]
-@show SelectAction(mdp, s, nSims, c)
+############
+# Question 3
+############
 
-N = Dict{Tuple{S, A}, Int}()
-Q = Dict{Tuple{S, A}, Float64}()
-t = Dict{Tuple{S, A, S}, Int}()
-d = 50;
-for k in 1:nSims
-    simulate!(mdp, s, d, c, N, Q, t)
+# mdp = DenseGridWorld(seed=4)
+# nsims = 7
+# c = 200
+# s = SA[19,19]
+# S = statetype(mdp)
+# A = actiontype(mdp)
+# N = Dict{Tuple{S, A}, Int}()
+# Q = Dict{Tuple{S, A}, Float64}()
+# T = Dict{Tuple{S, A, S}, Int}()
+# d = 50;
+# for k in 1:nSims
+#     simulate!(mdp, s, d, N, Q, T)
+# end
+
+# inchrome(visualize_tree(Q, N, T, SA[19,19])) # use inbrowser(visualize_tree(q, n, t, SA[1,1]), "firefox") etc. if you want to use a different browser
+
+############
+# Question 4
+############
+
+function SelectAction(mdp, s; nsims=nothing, tlim=nothing)
+    start = time_ns()
+
+    A = collect(actions(mdp))
+
+    N = Dict{Tuple{statetype(mdp), actiontype(mdp)}, Int}()
+    Q = Dict{Tuple{statetype(mdp), actiontype(mdp)}, Float64}()
+    d = 50;
+    if isnothing(tlim) && !isnothing(nsims)
+        for k in 1:nsims
+            simulate!(mdp, s, d, N, Q)
+        end
+    elseif !isnothing(tlim) && isnothing(nsims) 
+        while time_ns() < start + tlim
+            simulate!(mdp, s, d, N, Q)
+        end
+    else
+        throw(ArgumentError("Not enough input arguments for loop definition"))
+    end
+    return argmax(a->Q[(s,a)], A)
 end
 
-# @show typeof(s)
-# @assert s isa statetype(m)
+# @btime SelectAction(mdp, SA[35,35], nsims = 1000)
 
-# # here is an example of how to visualize a dummy tree (q, n, and t should actually be filled in your mcts code, but for this we fill it manually)
-# q[(SA[1,1], :right)] = 0.0
-# q[(SA[2,1], :right)] = 0.0
-# n[(SA[1,1], :right)] = 1
-# n[(SA[2,1], :right)] = 0
-# t[(SA[1,1], :right, SA[2,1])] = 1
+function rolloutQ4_rand(mdp, policy_function, s0, max_steps=100)
+    r_total = 0
+    t = 0
+    s = s0
+    while !isterminal(mdp, s) && t < max_steps
+        a = policy_function(m, s)
+        s, r = @gen(:sp, :r)(mdp, s, a)
+        r_total += r
+        t += 1
+    end
+    return r_total
+end
 
-inchrome(visualize_tree(Q, N, t, SA[19,19])) # use inbrowser(visualize_tree(q, n, t, SA[1,1]), "firefox") etc. if you want to use a different browser
+function rolloutQ4(mdp, policy_function, s0, n, max_steps=100)
+    r_total = 0
+    t = 0
+    s = s0
+    while !isterminal(mdp, s) && t < max_steps
+        a = policy_function(m, s, nsims=n)
+        s, r = @gen(:sp, :r)(mdp, s, a)
+        r_total += r
+        t += 1
+    end
+    return r_total
+end
+
+# iterations = 100;
+# results_random = [rolloutQ4_rand(mdp, random_policy, rand(initialstate(mdp))) for _ in 1:iterations]
+# mean_random, SEM_random = get_mean_SEM(results_random)
+# @show mean_random
+# # @show SEM_random
+
+# results_MCTS = [rolloutQ4(mdp, SelectAction, rand(initialstate(mdp)), 1000) for _ in 1:iterations]
+# mean_MCTS, SEM_MCTS = get_mean_SEM(results_MCTS)
+# @show mean_MCTS
+# @show SEM_MCTS
+# println("MCTS Policy Improvement: ", mean_MCTS-mean_random)
+
+############
+# Question 5
+############
+
+select_action(mdp,s) = SelectAction(mdp, s, tlim=40000000)
+HW3.evaluate(select_action, "amanda.marlow@colorado.edu", time=true)
